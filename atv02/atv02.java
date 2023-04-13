a9897deecb2c8935f2e7b0163b56e8d2c90b38ca
Jesus Alberto Sifontes Bermudez <115327702+sifontesj23@users.noreply.github.com>
2023-04-13 16:29:44 -0300

    Add files via upload

---
+++ atv02/atv02.java
@@ -0,0 +1,18 @@
+/*Crie uma classe em Java que realize cada uma das seguintes tarefas:
+a) Exibir a mensagem "Informe um inteiro: ", deixando o cursor na mesma linha;
+b) Atribuir o produto de variáveis b e c para a variável a;
+c) Utilizar um comentário para afirmar que um programa executa um cálculo de exemplo de folha de pagamento.
+ */
+package atv02;
+public class atv02 {
+    public static void main(String [] args) {
+        int a,b=2,c=3;
+
+        System.out.println("Informe um inteiro");
+        a = b*c;
+    
+        System.out.println("resultado do produto b*c=" + a + " atribuido a");
+       /*o programa executa um cálculo. por exemplo de folha de pagamento. */
+    }
+    
+}
